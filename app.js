892e0757a7848cdb640caaf0cf318fe66835d1ac
Think Synergy <thinksynergy@thinksynergy.co.ke>
2019-12-26 02:53:02 +0300

    Converting our Project to Handlebars

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-const express = require('express');
 const path = require('path');
 
-const adminData = require('./routes/admin')
-
-const shopRoutes = require('./routes/shop')
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
-app.set('view engine', 'pug')
-app.set('views', 'views')
 
-const bodyParser = require('body-parser');
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
-
 
 app.listen(3000);
