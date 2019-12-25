1e37b24b94101d4de6c130e484bd70eb980d6333
Think Synergy <thinksynergy@thinksynergy.co.ke>
2019-12-25 10:48:42 +0300

    Working with Handlebars

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const adminData = require('./routes/admin')
+
+const shopRoutes = require('./routes/shop')
+
+const app = express();
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'})
+});
+
+
+app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const rootDir = require('../util/path')
+
+const router = express.Router();
+
+const products = []
+
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+});
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    products.push({title: req.body.title})
+    res.redirect('/')
+});
+
+exports.routes = router
+exports.products = products
